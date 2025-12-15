322b4b110f0118ace7d9f71e924bb9632f2fea25
KimPot10 <jersonsagun1123@gmail.com>
2025-12-15 12:53:37 +0800

    lesson 2 sir flor

---
+++ lesson2.py
@@ -0,0 +1,25 @@
+#input format
+#num = input("Enter number: ")
+
+
+#lahat ng pini-print ni python ay automatic string
+#para gawing ibang data type, ito ang syntax
+
+""""
+num2 = int(input("Enter Number: "))
+
+print(num2)
+"""
+
+#output formatting in python
+#format() method
+
+#basic usage
+name = 'Jerson' 
+print("I'm {}".format(name))
+
+surname = 'Sagun'
+print("My Surname is: {}".format(surname))
+
+wholename = 'Jerson U. Sagun'
+print("My whole name is {}".format(surname))
