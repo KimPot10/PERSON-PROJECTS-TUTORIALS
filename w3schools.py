eabd461922bcb682bfd620926e525deac688fbc0
KimPot10 <jersonsagun1123@gmail.com>
2026-01-07 00:30:41 +0800

    can't focus

--- w3schools.py
+++ w3schools.py
@@ -1,1 +1,11 @@
 #   Many values to multiple threads
+
+x, y, z = "banana", "mango", "milktea"
+# para siyang array in a way. pero list siya... array nga? haha
+
+print(x)
+print(y)
+print(z)
+
+
+input("enter something: ")
