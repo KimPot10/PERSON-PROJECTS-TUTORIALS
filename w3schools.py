62f21b477bf9988c76215bd342dd5672db7813fb
KimPot10 <jersonsagun1123@gmail.com>
2026-01-08 23:30:52 +0800

    multiple variables in python

--- w3schools.py
+++ w3schools.py
@@ -1,3 +1,5 @@
+'''
+Docstring for w3schools
 #   Many values to multiple threads
 
 x, y, z = "banana", "mango", "milktea"
@@ -10,3 +12,22 @@ print(z)
 
 input("enter something: ")
 #git cant focus part 2
+'''
+
+#Assigning Multiple Values
+#unpack a collection.
+#basically para siyang array pero as alist? haha
+
+fruits = ["pineanpple", "strawberry", "banana"]
+a, b, c = fruits
+'''
+print(a)
+print(b)
+print(c)
+'''
+#other method ay pwede rin isang line
+
+print(a, b, c)
+
+#ooooo tangina isang line lang din sila mapiprint
+#aaaaa kasi potangina yung isang print ng python automatic na new line na haha bwisit amp
