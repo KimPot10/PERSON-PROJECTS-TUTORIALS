3e9a980453b36cd38892880a61ff1804be3603fc
KimPot10 <jersonsagun1123@gmail.com>
2026-01-10 22:36:03 +0800

    first problem solved. but with guidance of w3schools for if else

---
+++ problems/temperature.py
@@ -0,0 +1,30 @@
+temp = float(input("Enter a Temperature: "))
+
+conversion = input("Convert from (C)elsius or (F)ahrenheit?")
+
+#CELSIUS CONVERSION
+if conversion == "C":
+   
+    far = (temp* 9/5) + 32
+    far = float(far)
+    print("Celsius to fahrenheit is: " + str(far))
+
+elif conversion == "F":
+    cel = (temp- 32) * 5/9
+    cel = float(cel)
+    print("Fahrenheit to celsios is: " + str(cel))
+
+
+"""
+Learnings
+
+BE SURE ANONG GAGAMITING DATA TYPE HUWAG BASTA BASTA
+cel = float(input("Enter a Temperature: "))
+
+ALALAHANIN ANG CONVERSION DAHIL STR TYPE YAN LAHAT
+far = float(far)
+
+
+TypeError: can only concatenate str (not "float") to str. ALWAYS CONVERT INTO STR KAPAG MAG CONCATINATE
+print("Celsius to fahrenheit is: " + str(far))
+"""
