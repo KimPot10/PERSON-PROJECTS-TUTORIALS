fcc6c28835c62208b0749eca1c783e952c0c1b93
KimPot10 <jersonsagun1123@gmail.com>
2026-01-10 23:19:10 +0800

    if else temperature problem

--- problems/temperature.py
+++ problems/temperature.py
@@ -2,18 +2,23 @@ temp = float(input("Enter a Temperature: "))
 
 conversion = input("Convert from (C)elsius or (F)ahrenheit?")
 
-#CELSIUS CONVERSION
-if conversion == "C":
+#CELSIUS FAHRENHEIT
+if conversion == "F":
    
     far = (temp* 9/5) + 32
-    far = float(far)
+    far = float(cel)
     print("Celsius to fahrenheit is: " + str(far))
 
-elif conversion == "F":
+elif == "C":
     cel = (temp- 32) * 5/9
     cel = float(cel)
-    print("Fahrenheit to celsios is: " + str(cel))
+    print("Fahrenheit to celsius is: " + str(cel))
 
+#pwede rin ganito
+#elif coversion == "F": 
+#   cel = (temp- 32) * 5/9
+#   cel = float(cel)
+#   print("Fahrenheit to celsios is: " + str(cel))
 
 """
 Learnings
@@ -27,4 +32,11 @@ far = float(far)
 
 TypeError: can only concatenate str (not "float") to str. ALWAYS CONVERT INTO STR KAPAG MAG CONCATINATE
 print("Celsius to fahrenheit is: " + str(far))
+
+
+if condition :
+
+elif condition colon: 
+
+else:
 """
