8e395b649148edf0fe70f9437bce620b4abef5d8
KimPot10 <jersonsagun1123@gmail.com>
2026-01-12 23:17:19 +0800

    Grade checker

---
+++ problems/grade.py
@@ -0,0 +1,16 @@
+grade = int(input("Enter your grade: "))
+
+if grade < 100:
+    print("Your grade is A")
+elif grade < 90:
+    print("Your grade is B")
+elif grade < 80:
+    print("Your grade is C")
+elif grade < 70:
+    print("Your grade is D")
+elif grade > 100:
+    print("Invalid")
+elif grade < 69:
+    print("Invalid")
+elif grade 
+    print("Your grade is F")
