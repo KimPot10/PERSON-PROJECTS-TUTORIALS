7e173a37c15239e6c63a0439df564ebe15cb52e0
KimPot10 <jersonsagun1123@gmail.com>
2026-01-13 20:33:15 +0800

    Mali logic

--- problems/grade.py
+++ problems/grade.py
@@ -1,16 +1,20 @@
+"""
+
+MALI
+
 grade = int(input("Enter your grade: "))
 
-if grade < 100:
+if grade == 91 < 100:
     print("Your grade is A")
 elif grade < 90:
     print("Your grade is B")
 elif grade < 80:
     print("Your grade is C")
-elif grade < 70:
+elif grade <= 70:
     print("Your grade is D")
-elif grade > 100:
+elif grade > 101:
     print("Invalid")
-elif grade < 69:
-    print("Invalid")
-elif grade 
+else: 
     print("Your grade is F")
+
+"""
