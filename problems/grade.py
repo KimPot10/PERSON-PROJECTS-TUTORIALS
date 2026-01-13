a87fcf7878ade715b927888d8c478c8263f3b429
KimPot10 <jersonsagun1123@gmail.com>
2026-01-13 22:56:46 +0800

    Correct code

--- problems/grade.py
+++ problems/grade.py
@@ -18,3 +18,18 @@ else:
     print("Your grade is F")
 
 """
+
+grade = int(input("Enter your grade: "))
+# This states that grades  more than zero OR grades more than 100 is incorrect
+if grade < 0 or grade > 100:
+    print("Invalid Grade")
+# this states grade 90 to 100 are A
+elif grade >= 90:
+    print("Your grade is A")
+# this states 
+elif grade >= 80:
+    print("Your grade is B")
+elif grade >= 70:
+    print("Your grade is C")
+elif grade >= 60:
+    print("Your grade is F")
