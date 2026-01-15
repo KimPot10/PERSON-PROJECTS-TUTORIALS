543be20204d30cceec5aaa6b95de228c60a0bc87
KimPot10 <jersonsagun1123@gmail.com>
2026-01-15 23:48:56 +0800

    learning loops

--- problems/loops.py
+++ problems/loops.py
@@ -1,8 +1,21 @@
 #for is used of loops
 
 #mayroon din while
-
+"""
 for i in range(10):
     print(i)  # Prints 0 to 4
-
+"""
 #Commit kasi baka masira streak
+import random
+
+dice = random.randint(1,6)
+print(dice)
+count = 1
+
+while dice != 6:
+  dice = random.randint(1,6)
+  print(dice)
+  count += 1
+print('You got 6!')
+print('You rolled',count,'times')
+
