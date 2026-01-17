7ad4166de04cf3106fba5bbfc0df8fe8678c606e
KimPot10 <jersonsagun1123@gmail.com>
2026-01-17 22:42:31 +0800

    learning loops with stars ay pyramid. learning the concept of iteration

---
+++ problems/stars.py
@@ -0,0 +1,9 @@
+#printing stars in python using loop
+
+# "i" is oftenly used because of its meaning iteration meaning to repeat. range is number of how many times it will be repeated
+for i in range (1, 6):
+    #since nasa loob siya ng indent mag start na rin ito ng iteration
+    print('*' * i)
+    # asterisk multiplied by the iteration. kada row ma-multiply siya
+
+#without ai. paano mo siya gagawing pyramid?
